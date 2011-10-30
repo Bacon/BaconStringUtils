0f3047c43c5e3ba88d036558f0ef5b7473bd4fea
Ben Scholzen <mail@dasprids.de>
2011-10-30 02:33:34 +0100

    Fixed classmap

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -16,6 +16,6 @@
  */
 
 return array (
-  'Bacon\\Text\\UniDecode\\UniDecode' => __DIR__ . DIRECTORY_SEPARATOR . '' . DIRECTORY_SEPARATOR . 'home' . DIRECTORY_SEPARATOR . 'dasprid' . DIRECTORY_SEPARATOR . 'dev' . DIRECTORY_SEPARATOR . 'bacon' . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Bacon' . DIRECTORY_SEPARATOR . 'Text' . DIRECTORY_SEPARATOR . 'UniDecode' . DIRECTORY_SEPARATOR . 'UniDecode.php',
-  'Bacon\\Text\\Slugifier\\Slugifier' => __DIR__ . DIRECTORY_SEPARATOR . '' . DIRECTORY_SEPARATOR . 'home' . DIRECTORY_SEPARATOR . 'dasprid' . DIRECTORY_SEPARATOR . 'dev' . DIRECTORY_SEPARATOR . 'bacon' . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Bacon' . DIRECTORY_SEPARATOR . 'Text' . DIRECTORY_SEPARATOR . 'Slugifier' . DIRECTORY_SEPARATOR . 'Slugifier.php',
+  'Bacon\Text\UniDecode\UniDecode' => __DIR__ . '/src/Bacon/Text/UniDecode/UniDecode.php',
+  'Bacon\Text\Slugifier\Slugifier' => __DIR__ . '/src/Bacon/Text/Slugifier/Slugifier.php',
 );
